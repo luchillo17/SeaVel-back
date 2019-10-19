ed9a61f337af7ad945a45aa67c9d7d20149a16ba
luchillo17 <luchillo17@gmail.com>
2019-10-19 18:11:08 -0500

    feat: memoize python script results

--- src/SeaVel.js
+++ src/SeaVel.js
@@ -1,46 +1,56 @@
 const { PythonShell } = require('python-shell');
 const path = require('path');
+const memoize = require('memoizee');
 
 /**
  *
  * @param {location} param0
  */
 module.exports = {
-  SeaVel({ lon, lat, city, date, freq }) {
-    let options = {
-      mode: 'text',
-      pythonPath: path.resolve(__dirname, '..', 'venv', 'python.exe'),
-      pythonOptions: ['-u'], // get print results in real-time
-      scriptPath: '.',
-      args: [
-        JSON.stringify({
-          lon,
-          lat,
-          city,
-          date,
-          freq
-        })
-      ]
-    };
+  SeaVel: memoize(
+    function({ lon, lat, city, date, freq }) {
+      let options = {
+        mode: 'text',
+        pythonPath: path.resolve(__dirname, '..', 'venv', 'python.exe'),
+        pythonOptions: ['-u'], // get print results in real-time
+        scriptPath: '.',
+        args: [
+          JSON.stringify({
+            lon,
+            lat,
+            city,
+            date,
+            freq
+          })
+        ]
+      };
 
-    return new Promise((resolve, reject) => {
-      PythonShell.run(
-        path.resolve(__dirname, 'SeaVel-maps.py'),
-        options,
-        (err, output) => {
-          if (err) {
-            reject(err);
+      return new Promise((resolve, reject) => {
+        PythonShell.run(
+          path.resolve(__dirname, 'SeaVel-maps.py'),
+          options,
+          (err, output) => {
+            if (err) {
+              reject(err);
+            }
+
+            try {
+              const result = JSON.parse(output);
+
+              resolve(result);
+            } catch (error) {
+              reject(error);
+            }
           }
-
-          try {
-            const result = JSON.parse(output);
-
-            resolve(result);
-          } catch (error) {
-            reject(error);
-          }
-        }
-      );
-    });
-  }
+        );
+      });
+    },
+    {
+      normalizer: function(args) {
+        // args is arguments object as accessible in memoized function
+        console.log('Memoize args: ', args);
+        return JSON.stringify(args[0]);
+      }
+    }
+  )
 };
